a974bdc95a2b8145e436ff019800c9eb1f765cba
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-13 12:06:27 -0600

    PostCreate.js

--- src/PostCreate.js
+++ src/PostCreate.js
@@ -6,10 +6,22 @@ import React, { useState } from "react";
  * When the form is submitted, a new post is created and the form contents cleared.
  */
 function PostCreate() {
-  const [type, setType] = useState("Text");
-
+  const initialFormState = {
+    type: "Text",
+    content: "",
+  };
+  const [formState, setFormState] = useState({ ...initialFormState });
+  const handleChange = ({target}) => {
+    // console.log(formState, target.name, target.value);
+    // console.log(formState);
+    // console.log(event);
+    setFormState({ ...formState, [target.name]: target.value });
+  };
   // TODO: When the form is submitted, a new post should be created, and the form contents cleared.
-
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFormState({ ...initialFormState });
+  };
   // For the tests to pass, the form below must have:
   // - a `name="create"` attribute
   // - one child `<button>` with a `type="submit"` attribute
@@ -17,22 +29,42 @@ function PostCreate() {
   // - one child `<textarea>` or `<input>` (not both at the same time) with a `name="content"`
 
   return (
-    <form name="create">
+    <form name="create" onSubmit={handleSubmit}>
       <fieldset>
         <legend>Create</legend>
         <div>
           <label htmlFor="type">Type: </label>
-          <select id="type" name="type" required={true}>
+          <select
+            id="type"
+            name="type"
+            required={true}
+            onChange={handleChange}
+            value={formState.type}
+          >
             <option>Text</option>
             <option>Image</option>
           </select>
         </div>
         <div>
           <label htmlFor="content">Content: </label>
-          {type === "Text" ? (
-            <textarea id="content" name="content" required={true} rows={3} />
+          {formState.type === "Text" ? (
+            <textarea
+              id="content"
+              name="content"
+              required={true}
+              rows={3}
+              onChange={handleChange}
+              value={formState.content}
+            />
           ) : (
-            <input id="content" name="content" type="url" required={true} />
+            <input
+              id="content"
+              name="content"
+              type="url"
+              required={true}
+              onChange={handleChange}
+              value={formState.content}
+            />
           )}
         </div>
         <div>
