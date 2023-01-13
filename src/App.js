d936df60a1c956595c075fbf89e14395f6dee468
MariahCasciola <117303840+MariahCasciola@users.noreply.github.com>
2023-01-13 14:37:23 -0600

    added props to PostCreate

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <PostCreate />
+      <PostCreate posts={posts} setPosts={setPosts} />
       <PostList posts={posts} setPosts={setPosts}/>
     </div>
   );
--- src/PostList.js
+++ src/PostList.js
@@ -6,6 +6,7 @@ import React from "react";
  */
 function PostList() {
   // TODO: Diplay the list of posts.
+  //populate the empty array from App
   // TODO: Create at least one additional component that is used by this component.
   // TODO: Each post must have className="post" for the tests to work.
   // TODO: Each post must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
